ec34235afc2bc1413d7047c675df11b0c0fd5c1e
Jordan Cronier <jcronier777@gmail.com>
2021-09-15 20:19:29 +0000

    Added countLetters.js which counts the number of letters in a given sentence and returns those counts as an object

---
+++ countLetters.js
@@ -0,0 +1,42 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  let letterCounts = {};
+  for (const letter of sentence.split(" ").join("")) {
+    if (letterCounts[letter]) {
+      letterCounts[letter]++;
+    } else if (!letterCounts[letter]) {
+      letterCounts[letter] = 1;
+    }
+  }
+  return letterCounts;
+};
+
+const verifyCounts = {
+  l: 1,
+  i: 2,
+  g: 1,
+  h: 4,
+  t: 2,
+  o: 2,
+  u: 2,
+  s: 2,
+  e: 3,
+  n: 1,
+};
+
+const sentence = "lighthouse in the house";
+
+const letterCounts = countLetters(sentence);
+console.log(letterCounts);
+
+assertEqual(Object.keys(letterCounts).length, Object.keys(verifyCounts).length);
+for (const key in verifyCounts) {
+  assertEqual(letterCounts[key], verifyCounts[key]);
+}
