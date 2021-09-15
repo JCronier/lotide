c4bd92ff9c73e155c9579acd54b191c4d03857f7
Jordan Cronier <jcronier777@gmail.com>
2021-09-15 22:49:01 +0000

    Changed eqObjects at behest of Compass

--- eqObjects.js
+++ eqObjects.js
@@ -13,7 +13,7 @@ const eqObjects = function(object1, object2) {
     return false;
   }
 
-  for (const key in object1) {
+  for (const key of Object.keys(object1)) {
     
     if (object1[key] !== object2[key]) {
       return false;
