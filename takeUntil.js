1670f06cb07ab635de85f5d8cf1ea9da8c701934
Jordan Cronier <jcronier777@gmail.com>
2021-09-16 17:11:12 +0000

    Fixed takeUntil to not include the truthy valueinto output array

--- takeUntil.js
+++ takeUntil.js
@@ -2,10 +2,11 @@ const takeUntil = function(array, callback) {
   let results = [];
 
   for (const element of array) {
-    results.push(element);
     if (callback(element)) {
       break;
     }
+    results.push(element);
+   
   }
 
   return results;
@@ -37,13 +38,13 @@ const eqArrays = function(firstArray, secondArray) {
 
 
 /* #region  Test Code */
-const data1 = [1, 2, 5, 7, 2, 1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x > 5);
-assertArraysEqual(results1, [1, 2, 5, 7]);
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
 
 console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-assertArraysEqual(results2, [ "I've", 'been', 'to', 'Hollywood', ',' ]);
+assertArraysEqual(results2, [ "I've", 'been', 'to', 'Hollywood']);
 /* #endregion */
