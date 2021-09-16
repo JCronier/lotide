2715d34a340aa5dae6cc156dbed11e9136a629a2
Jordan Cronier <jcronier777@gmail.com>
2021-09-16 17:36:14 +0000

    Added findKey function to find the key of the first property of an object that satisfies a given condition

---
+++ findKey.js
@@ -0,0 +1,58 @@
+const findKey = function (object, callback) {
+
+  for (const key in object) {
+
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  // return Object.keys(object).find(key => callback(object[key]));
+};
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+    return;
+  }
+
+  console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+
+};
+
+/* #region  Test Code */
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2), "noma");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 1), "Blue Hill");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars > 3), undefined);
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { likes: 2 },
+  "noma": { stars: 2 },
+  "elBulli": { likes: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 3), "Akelarre");
+/* #endregion */
